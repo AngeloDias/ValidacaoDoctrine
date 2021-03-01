9477977c14d84197d3f098b57684fb6bdeb54799
angelodias <angelossdias@gmail.com>
2021-03-01 18:02:49 -0300

    feat: fazendo update e listagem

--- src/Controller/AlunoController.php
+++ src/Controller/AlunoController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\AlunoType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,6 +14,73 @@ use App\Entity\Aluno;
 
 class AlunoController extends AbstractController
 {
+     /**
+     * @Route("/aluno/editar/{id}", name="aluno_editar")
+     */
+    public function update(Request $request, int $id): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $aluno = $entityManager->getRepository(Aluno::class)->find($id);
+
+        if ($request->isMethod('POST')) {
+            $aluno->setNome($request->request->get('nome'));
+            $aluno->setCurso($request->request->get('curso'));
+
+            $entityManager->persist($aluno);
+
+            $entityManager->flush();
+
+            return new Response('O aluno foi atualizado com nome: '.$aluno->getNome());
+
+            // return $this->redirectToRoute('aluno_show', [
+            //     'id' => $aluno->getId()
+            // ]);
+        }
+
+        $form = $this->createForm(AlunoType::class, $aluno);
+        $form->handleRequest($request);
+
+        return $this->render('aluno/editar.html.twig', [
+            'aluno' => $aluno,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/aluno/{id}", name="aluno_show")
+     */
+    public function show(int $id): Response
+    {
+        $aluno = $this->getDoctrine()
+            ->getRepository(Aluno::class)
+            ->find($id);
+
+        if (!$aluno) {
+            throw $this->createNotFoundException(
+                'Nenhum aluno encontrado com o ID: '.$id
+            );
+        }
+
+        return new Response('O nome do aluno selecionado: '.$aluno->getNome());
+
+        // return $this->render('aluno/show.html.twig', [
+        //     'aluno' => $aluno,
+        // ]);
+    }
+
+    /**
+     * @Route("/listar", name="listar_alunos")
+     */
+    public function listar(): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        return $this->render('aluno/listar.html.twig', [
+            'alunos' => $entityManager->getRepository(Aluno::class)->findAll(),
+            'errors' => ''
+        ]);
+    }
+
     /**
      * @Route("/", name="aluno")
      */
